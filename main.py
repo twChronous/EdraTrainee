9ff722de980a96d98a1960b8734b675f31a9418b
Lucas Mateus <lucasmateustds@gmail.com>
2023-12-03 11:39:52 -0300

    :bulb: raw: mais dados adicionados no treinamento

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-import os
-import cv2
 from ultralytics import YOLO
-model_path = os.path.join('.', 'train', 'runs', 'detect', 'train2', 'weights', 'last.pt')
+import cv2
+import cvzone
+import math
+import os
 
+model_path = os.path.join('.', 'train', 'runs', 'detect', 'train', 'weights', 'last.pt')
+cap = cv2.VideoCapture(1)
+model = YOLO(model_path) 
 
-#model = YOLO(model_path)  
+classNames = ["base"]
 
-def exibir_webcam():
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Erro ao abrir a webcam.")
-        return
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
 
-    while True:
-        ret, frame = cap.read()
+            # Bounding Box
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(img, (x1,y1),(x2,y2),(255,0,255),3)
 
-        if not ret:
-            print("Erro ao capturar o frame.")
-            break
+            w, h, = x2 - x1, y2 - y1
+            cvzone.cornerRect(img,(x1,y1,w,h))
 
-        cv2.imshow('Webcam', frame)
+            # Confidence
+            conf = math.ceil((box.conf[0] *100))
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-print(model_path)
-exibir_webcam()
-
-
-
+            # Class Name
+            cls = int(box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}%', (max(0, x1), max(35, y1)),scale=0.9,thickness=2)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
--- train/main.py
+++ train/main.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.yaml") 
-model.train(data="config.yaml", epochs=200)  
+model.train(data="config.yaml", epochs=200)
+model.val()
